04145c74a1728c708c871922d04152ca28805d39
whizyprojetos <whizysafary@gmail.com>
2026-01-09 20:50:49 +0100

    Add files via upload

---
+++ arquivos.js
@@ -0,0 +1,18 @@
+/**
+ * WHIZYNET - Banco de Dados de Arquivos VPN
+ * Adicione novos arquivos de configuração aqui.
+ */
+
+const FILES_DATA = [
+    {
+        id: 1,
+        name: "WHIZYNET-INJ1",
+        app: "HTTP Injector",
+        extension: ".ehi",
+        category: "Angola",
+        date: "10 Jan 2026",
+        size: "10.26 KB",
+        downloadUrl: "https://github.com/whizyprojetos/whizynet/blob/480ddc50a5a1c0f6aa8236d53338907d771fdb25/WHIZYNET-INJ1.ehi",
+        description: "Configuração estável para Unitel. Alta velocidade Expira: 17 jan 2026."
+    }
+];

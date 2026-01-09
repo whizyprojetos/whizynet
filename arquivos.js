987e0fba2b0285770274ee74aa2281487b80ff49
whizyprojetos <whizysafary@gmail.com>
2026-01-10 03:06:09 +0700

    Add files via upload

--- arquivos.js
+++ arquivos.js
@@ -6,13 +6,24 @@
 const FILES_DATA = [
     {
         id: 1,
+        name: "WHIZYNET-HAT1",
+        app: "HA Tunnel Plus",
+        extension: ".hat",
+        category: "Angola",
+        date: "10 Jan 2026",
+        size: "1.71 KB",
+        downloadUrl: "https://www.mediafire.com/file/3y0yid41sauacpi/WHIZYNET-HAT1.hat/file",
+        description: "Configuração estável para Unitel. Alta velocidade Expira: 17 jan 2026."
+    },
+    {
+        id: 2,
         name: "WHIZYNET-INJ1",
         app: "HTTP Injector",
         extension: ".ehi",
         category: "Angola",
         date: "10 Jan 2026",
         size: "10.26 KB",
-        downloadUrl: "https://github.com/whizyprojetos/whizynet/blob/480ddc50a5a1c0f6aa8236d53338907d771fdb25/WHIZYNET-INJ1.ehi",
+        downloadUrl: "https://www.mediafire.com/file/4e83bjmp3in4hvc/WHIZYNET-INJ1.ehi/file",
         description: "Configuração estável para Unitel. Alta velocidade Expira: 17 jan 2026."
     }
 ];

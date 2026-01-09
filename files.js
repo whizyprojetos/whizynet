674f8c22a339203b999e881bf77aa60d60baccd4
whizyprojetos <whizysafary@gmail.com>
2026-01-10 03:12:34 +0700

    Add files via upload

---
+++ files.js
@@ -0,0 +1,29 @@
+/**
+ * WHIZYNET - Banco de Dados de Arquivos VPN
+ * Adicione novos arquivos de configuração aqui.
+ */
+
+const FILES_DATA = [
+    {
+        id: 1,
+        name: "WHIZYNET-HAT1",
+        app: "HA Tunnel Plus",
+        extension: ".hat",
+        category: "Angola",
+        date: "10 Jan 2026",
+        size: "1.71 KB",
+        downloadUrl: "https://www.mediafire.com/file/3y0yid41sauacpi/WHIZYNET-HAT1.hat/file",
+        description: "Configuração estável para Unitel. Alta velocidade Expira: 17 jan 2026."
+    },
+    {
+        id: 2,
+        name: "WHIZYNET-INJ1",
+        app: "HTTP Injector",
+        extension: ".ehi",
+        category: "Angola",
+        date: "10 Jan 2026",
+        size: "10.26 KB",
+        downloadUrl: "https://www.mediafire.com/file/4e83bjmp3in4hvc/WHIZYNET-INJ1.ehi/file",
+        description: "Configuração estável para Unitel. Alta velocidade Expira: 17 jan 2026."
+    }
+];

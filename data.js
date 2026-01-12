74dfbdaafe1c74e0b2d75e28c4c41d5b0fc134a7
whizyprojetos <whizysafary@gmail.com>
2026-01-12 19:46:58 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 // WHIZYNET - Dados de SNI por País e Operador
-// SNIs Válidas e Testadas
+// SNIs Válidas e Testadas - Atualizado em 12/01/2026
 
 const SNI_DATA = {
     "Angola": {
@@ -16,44 +16,130 @@ const SNI_DATA = {
             "unitelcg.giftedus.online",
             "unitelmoney.ao",
             "ib.bancobai.ao",
-            "angolamove.unitel.ao"
+            "angolamove.unitel.ao",
+            "graph.facebook.com"
         ],
         "Movicel": [
-            "movicel.ao",
-            "movicel.co.ao",
             "internet.movicel.co.ao",
             "162.159.128.79",
-            "pagamentonline.emis.co.ao",
+            "movicel.co.ao",
             "www.facebook.com",
+            "pagamentonline.emis.co.ao",
+            "movicel.ao"
         ],
         "Africell": [
-            "africell.ao",
-            "91.220.208.64",
-            "www.mcexpress.emis.com",
             "care.africell.ao",
-            "premierbetzone.com",
             "pagamentonline.emis.co.ao",
+            "91.220.208.64",
+            "premierbetzone.com",
+            "www.mcexpress.emis.com",
+            "africell.ao"
         ]
     },
     "Moçambique": {
         "Vodacom": [
-            "www.vodacom.co.mz",
-            "webmail.vodacom.co.mz",
+            "www.misau.gov.mz",
+            "www.mined.gov.mz",
+            "app.ubuntu.vm.co.mz",
             "portal.vodacom.co.mz",
+            "webmail.vodacom.co.mz",
+            "www.vodacom.co.mz",
             "www.vodacomdigital.co.mz",
-            "app.vodacom.co.mz",
-            "mail.vodacom.co.mz",
-            "mobile.vodacom.co.mz",
-            "www.vodacomnet.co.mz",
-            "email.vodacom.co.mz",
-            "www.vodacomwifi.co.mz"
+            "app.vodacom.co.mz"
         ],
         "Movitel": [
-            "img.zakelijkfinancieren-a1.blgwonen.nl",
-            "app.ubuntu.vm.co.mz"
+            "movitel.co.mz",
+            "www.movitel.co.mz",
+            "app.movitel.co.mz",
+            "img.zakelijkfinancieren-a1.blgwonen.nl"
         ],
         "Tmcel": [
-            "www.tmcel.mz"
+            "www.tmcel.mz",
+            "mail.tmcel.mz"
+        ]
+    },
+    "Brasil": {
+        "Vivo": [
+            "www.vivo.com.br",
+            "login.vivo.com.br",
+            "meu.vivo.com.br",
+            "m.facebook.com",
+            "whatsapp.net"
+        ],
+        "Claro": [
+            "www.claro.com.br",
+            "minha.claro.com.br",
+            "claro.com.br",
+            "m.facebook.com",
+            "whatsapp.net"
+        ],
+        "Tim": [
+            "www.tim.com.br",
+            "meu.tim.com.br",
+            "tim.com.br",
+            "m.facebook.com",
+            "whatsapp.net"
+        ]
+    },
+    "Nigéria": {
+        "MTN": [
+            "mtnonline.com",
+            "www.mtn.ng",
+            "ncdc.gov.ng",
+            "who.int",
+            "wikipedia.org",
+            "facebook.com"
+        ],
+        "Airtel": [
+            "airtel.com.ng",
+            "www.airtel.com.ng",
+            "airtelstream.net",
+            "unicef.org"
+        ],
+        "Glo": [
+            "gloworld.com",
+            "www.gloworld.com",
+            "glo.com.ng"
+        ],
+        "9mobile": [
+            "9mobile.com.ng",
+            "www.9mobile.com.ng"
+        ]
+    },
+    "Ghana": {
+        "MTN": [
+            "mtn.com.gh",
+            "www.mtn.com.gh",
+            "ayoba.me"
+        ],
+        "Vodafone": [
+            "vodafone.com.gh",
+            "at.com.gh",
+            "www.at.com.gh"
+        ],
+        "AirtelTigo": [
+            "airteltigo.com.gh",
+            "www.airteltigo.com.gh"
+        ]
+    },
+    "África do Sul": {
+        "Vodacom": [
+            "vodacom.co.za",
+            "www.vodacom.co.za",
+            "connectu.vodacom.co.za"
+        ],
+        "MTN": [
+            "mtn.co.za",
+            "www.mtn.co.za",
+            "ayoba.me"
+        ],
+        "Cell C": [
+            "cellc.co.za",
+            "www.cellc.co.za"
+        ],
+        "Telkom": [
+            "telkom.co.za",
+            "www.telkom.co.za"
         ]
     }
-};
+ };
